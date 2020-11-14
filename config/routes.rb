e3f62936bbed137161b9e2fd25376469c8995a25
eriko0906 <per.music.ef.2@gmail.com>
2020-11-14 10:21:58 +0900

    home削除

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@ Rails.application.routes.draw do
   get 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/about'
-  get 'home/top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
